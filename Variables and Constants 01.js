18f79f14a65c9cf7fa994d88a8c65f9f93c05661
TALHA7507 <TALHARIAZ750674@gmail.com>
2025-05-08 01:08:15 +0500

    Variables and Constants

---
+++ Variables and Constants 01.js
@@ -0,0 +1,21 @@
+const account_id = 266241
+let account_email = " gul3545792@gmail.com "
+var account_password = " 750750 "
+account_city = " Lahore "
+let account_state;
+
+// account_id = 750
+account_email = " talhariaz750674@gmail.com "
+account_password = " 13579 "
+account_id
+
+/*
+prefer not use 'var' because main issue in block scope and functional scope. Simple scope like { }
+*/
+
+
+// console.log(account_email);
+// console.log(account_password);
+
+
+console.table([account_id, account_email, account_id, account_password, account_city, account_state]);
