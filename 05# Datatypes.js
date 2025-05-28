3b7028a33607e318850ec4f702ea40838566071f
Muhammad Talha <TALHARIAZ750674@gmail.com>
2025-05-28 20:11:53 +0000

    DataTypes

---
+++ 05# Datatypes.js
@@ -0,0 +1,23 @@
+let name = "John Doe";
+let age = 30;                   
+let isloggedin = true;
+let = state = "undefined"; // This is a syntax error, should be 'let state = "undefined";'
+let user = null; // This is a valid way to initialize a variable with no value
+
+//datatypes
+
+let number = 42; // Number type
+let string = "Hello, World!"; // String type
+let boolean = true; // Boolean type
+let object = { key: "value" }; // Object type
+let array = [1, 2, 3]; // Array type
+let func = function() { return "I am a function"; }; // Function type
+let symbol = Symbol("unique"); // Symbol type
+let bigInt = BigInt(123456789012345678901234567890); // BigInt type  or very long integers
+
+
+// Objects
+
+console.log (typeof " TALHA ");          // Tells the type isString
+console.log (typeof  null );          // Tells the type is Object
+console.log (typeof  undefined );    // Tells the type is Undefined 
