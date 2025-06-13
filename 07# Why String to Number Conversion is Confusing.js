8c3a8fcfcb414e1b04ee768c8cda294d323b0d3a
Muhammad Talha <TALHARIAZ750674@gmail.com>
2025-06-13 11:40:37 +0000

    Number and String Conversion

---
+++ 07# Why String to Number Conversion is Confusing.js
@@ -0,0 +1,46 @@
+let string = "750";
+let negvalue = -string;
+console.log(negvalue);
+
+
+// Operations
+
+console.log(3 + 5);
+console.log(5 + 4);
+console.log("4" + "1");
+console.log("1" + 5);
+console.log(2 + "3");
+console.log(2 - 3);
+console.log("5" - "3");
+console.log("6" - 7);
+console.log(9 / 3);
+console.log("1" / 4);
+console.log("5" * "3");
+console.log("5" * 3);
+console.log("5" * "3" + 2);
+console.log("5" * "3" + "2");
+console.log("5" * "3" + "2" - 1);
+console.log("1" * "3" + "2" - "1");
+console.log(2 % 5);
+console.log(5 % 2);
+console.log("2" % "5");
+console.log("2" % 5);
+console.log(2 % "5");
+console.log(5 ** 2);
+console.log("5" ** "2");
+console.log("5" ** 2);
+console.log("5" ** "2" + 1);
+
+
+
+// String Operations
+
+let str1 = "Hello";
+let str2 = " TALHA";
+console.log(str1 +"  " + str2);
+console.log(str1 );
+console.log(str1 - str2); 
+console.log(str1 * str2);
+let str3 = str1 + str2;
+console.log(str3);
+console.log((5 + 2) - 1 * 6 % 5 ** 4 / 1);
